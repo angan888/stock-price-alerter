e9e0121ef7e21f23e609577c8588909bbd10d22b
Albert <mtrip99@gmail.com>
2020-11-21 16:38:19 -0500

    by me

--- hello world.py
+++ hello world.py
@@ -5,9 +5,4 @@ import datetime
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("wayfair-price-history.csv")
-
-print(data.columns)
-print(data.describe)
-
-print(data['CAN-PICO-DESC'])
+print('hi imma in here now')
